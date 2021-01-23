038adb0133a82f0b09561f51ade18a154b87ee7c
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2021-01-23 08:41:58 -0300

    Add basic scriptlet execution test
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

---
+++ tests/integration/executor/test_runner.py
@@ -0,0 +1,53 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright (C) 2021 Canonical Ltd
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import textwrap
+
+import pytest
+import yaml
+
+import craft_parts
+from craft_parts import Action, ActionType, Step
+
+
+@pytest.mark.parametrize("step", [Step.PULL, Step.BUILD, Step.STAGE, Step.PRIME])
+@pytest.mark.parametrize(
+    "action_type",
+    [ActionType.RUN, ActionType.RERUN, ActionType.SKIP, ActionType.UPDATE],
+)
+def test_override(tmpdir, capfd, step, action_type):
+    parts_yaml = textwrap.dedent(
+        """\
+        parts:
+          foo:
+            plugin: nil
+            override-pull: echo "override Step.PULL"
+            override-build: echo "override Step.BUILD"
+            override-stage: echo "override Step.STAGE"
+            override-prime: echo "override Step.PRIME"
+        """
+    )
+
+    parts = yaml.safe_load(parts_yaml)
+    lf = craft_parts.LifecycleManager(parts, work_dir=tmpdir)
+
+    lf.execute(Action("foo", step, action_type=action_type))
+    out, err = capfd.readouterr()
+    assert not err
+    if action_type in [ActionType.SKIP, ActionType.UPDATE]:
+        assert not out
+    else:
+        assert out == f"override {step!r}\n"
