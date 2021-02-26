3a5fea78674cb417269a55c2876c095957ae6ac6
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2021-02-26 11:16:55 -0300

    Add execution prologue/epilogue integration tests
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- craft_parts/__init__.py
+++ craft_parts/__init__.py
@@ -19,6 +19,7 @@
 __version__ = "0.0.1"  # noqa: F401
 
 from .actions import Action, ActionType  # noqa: F401
-from .infos import StepInfo  # noqa: F401
+from .infos import PartInfo, ProjectInfo, StepInfo  # noqa: F401
 from .manager import ExecutionContext, LifecycleManager  # noqa: F401
+from .parts import Part  # noqa: F401
 from .steps import Step  # noqa: F401
--- tests/integration/executor/test_callbacks.py
+++ tests/integration/executor/test_callbacks.py
@@ -15,12 +15,22 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import textwrap
+from typing import List
 
 import pytest
 import yaml
 
 import craft_parts
-from craft_parts import Action, ActionType, Step, StepInfo, callbacks, errors
+from craft_parts import (
+    Action,
+    ActionType,
+    Part,
+    ProjectInfo,
+    Step,
+    StepInfo,
+    callbacks,
+    errors,
+)
 
 
 def setup_function():
@@ -31,7 +41,7 @@ def teardown_module():
     callbacks.clear()
 
 
-def _info_callback(info: StepInfo) -> bool:
+def _step_callback(info: StepInfo) -> bool:
     print(f"step = {info.step!r}")
     print(f"part_src_dir = {info.part_src_dir}")
     print(f"part_build_dir = {info.part_build_dir}")
@@ -39,13 +49,31 @@ def _info_callback(info: StepInfo) -> bool:
     return True
 
 
+def _exec_callback(info: ProjectInfo, part_list: List[Part]) -> None:
+    print(f"application_name = {info.application_name}")
+    print(f"parts = {', '.join([x.name for x in part_list])}")
+
+
+_parts_yaml = textwrap.dedent(
+    """\
+    parts:
+      foo:
+        plugin: nil
+        override-pull: echo "override Step.PULL"
+        override-build: echo "override Step.BUILD"
+        override-stage: echo "override Step.STAGE"
+        override-prime: echo "override Step.PRIME"
+    """
+)
+
+
 @pytest.mark.parametrize("step", list(Step))
-def test_callback_step_info(tmpdir, capfd, step):
-    callbacks.register_pre_step(_info_callback, step_list=[step])
+def test_step_callback(tmpdir, capfd, step):
+    callbacks.register_pre_step(_step_callback, step_list=[step])
 
     parts = yaml.safe_load(_parts_yaml)
     lf = craft_parts.LifecycleManager(
-        parts, application_name="test_callback", work_dir=tmpdir
+        parts, application_name="test_step_callback", work_dir=tmpdir
     )
 
     with lf.execution_context() as ctx:
@@ -62,20 +90,27 @@ def test_callback_step_info(tmpdir, capfd, step):
     )
 
 
-_parts_yaml = textwrap.dedent(
-    """\
-    parts:
-      foo:
-        plugin: nil
-        override-pull: echo "override Step.PULL"
-        override-build: echo "override Step.BUILD"
-        override-stage: echo "override Step.STAGE"
-        override-prime: echo "override Step.PRIME"
-    """
-)
+def test_prologue_callback(tmpdir, capfd):
+    callbacks.register_prologue(_exec_callback)
+
+    parts = yaml.safe_load(_parts_yaml)
+    lf = craft_parts.LifecycleManager(
+        parts, application_name="test_prologue_callback", work_dir=tmpdir
+    )
+
+    with lf.execution_context() as ctx:
+        ctx.execute(Action("foo", Step.PULL))
+
+    out, err = capfd.readouterr()
+    assert not err
+    assert out == (
+        "application_name = test_prologue_callback\n"
+        "parts = foo\n"
+        "override Step.PULL\n"
+    )
 
 
-def _my_callback(info: StepInfo) -> bool:
+def _my_step_callback(info: StepInfo) -> bool:
     msg = getattr(info, "message")
     print(msg)
     return True
@@ -88,7 +123,7 @@ def _my_callback(info: StepInfo) -> bool:
 @pytest.mark.parametrize("step", list(Step))
 @pytest.mark.parametrize("action_type", list(set(ActionType) - {ActionType.UPDATE}))
 def test_callback_pre(tmpdir, capfd, step, action_type):
-    callbacks.register_pre_step(_my_callback, step_list=[step])
+    callbacks.register_pre_step(_my_step_callback, step_list=[step])
 
     parts = yaml.safe_load(_parts_yaml)
     lf = craft_parts.LifecycleManager(
@@ -109,7 +144,7 @@ def test_callback_pre(tmpdir, capfd, step, action_type):
 @pytest.mark.parametrize("step", list(Step))
 @pytest.mark.parametrize("action_type", list(set(ActionType) - {ActionType.UPDATE}))
 def test_callback_post(tmpdir, capfd, step, action_type):
-    callbacks.register_post_step(_my_callback, step_list=[step])
+    callbacks.register_post_step(_my_step_callback, step_list=[step])
 
     parts = yaml.safe_load(_parts_yaml)
     lf = craft_parts.LifecycleManager(
@@ -143,7 +178,7 @@ _update_yaml = textwrap.dedent(
 
 @pytest.mark.parametrize("step", [Step.PULL, Step.BUILD])
 def test_update_callback_pre(tmpdir, capfd, step):
-    callbacks.register_pre_step(_my_callback, step_list=[step])
+    callbacks.register_pre_step(_my_step_callback, step_list=[step])
 
     parts = yaml.safe_load(_update_yaml)
     lf = craft_parts.LifecycleManager(
@@ -160,7 +195,7 @@ def test_update_callback_pre(tmpdir, capfd, step):
 
 @pytest.mark.parametrize("step", [Step.PULL, Step.BUILD])
 def test_update_callback_post(tmpdir, capfd, step):
-    callbacks.register_post_step(_my_callback, step_list=[step])
+    callbacks.register_post_step(_my_step_callback, step_list=[step])
 
     parts = yaml.safe_load(_update_yaml)
     lf = craft_parts.LifecycleManager(
@@ -177,7 +212,7 @@ def test_update_callback_post(tmpdir, capfd, step):
 
 @pytest.mark.parametrize("step", [Step.STAGE, Step.PRIME])
 def test_invalid_update_callback_pre(tmpdir, step):
-    callbacks.register_pre_step(_my_callback, step_list=[step])
+    callbacks.register_pre_step(_my_step_callback, step_list=[step])
 
     parts = yaml.safe_load(_update_yaml)
     lf = craft_parts.LifecycleManager(
@@ -195,7 +230,7 @@ def test_invalid_update_callback_pre(tmpdir, step):
 
 @pytest.mark.parametrize("step", [Step.STAGE, Step.PRIME])
 def test_invalid_update_callback_post(tmpdir, step):
-    callbacks.register_post_step(_my_callback, step_list=[step])
+    callbacks.register_post_step(_my_step_callback, step_list=[step])
 
     parts = yaml.safe_load(_update_yaml)
     lf = craft_parts.LifecycleManager(
@@ -209,3 +244,59 @@ def test_invalid_update_callback_post(tmpdir, step):
     assert (
         str(raised.value) == f"Action is invalid: cannot update step {name!r} of 'foo'."
     )
+
+
+def _my_exec_callback(info: ProjectInfo, part_list: List[Part]) -> None:
+    for part in part_list:
+        print(f"{part.name}: {getattr(info, 'message')}")
+
+
+_exec_yaml = textwrap.dedent(
+    """\
+    parts:
+      foo:
+        plugin: nil
+        override-pull: echo "foo Step.PULL"
+        override-build: echo "foo Step.BUILD"
+        override-stage: echo "foo Step.STAGE"
+        override-prime: echo "foo Step.PRIME"
+      bar:
+        plugin: nil
+        override-pull: echo "bar Step.PULL"
+        override-build: echo "bar Step.BUILD"
+        override-stage: echo "bar Step.STAGE"
+        override-prime: echo "bar Step.PRIME"
+    """
+)
+
+
+def test_callback_prologue(tmpdir, capfd):
+    callbacks.register_prologue(_my_exec_callback)
+
+    parts = yaml.safe_load(_exec_yaml)
+    lf = craft_parts.LifecycleManager(
+        parts, application_name="test_callback", work_dir=tmpdir, message="prologue"
+    )
+
+    with lf.execution_context() as ctx:
+        ctx.execute(Action("foo", Step.PULL))
+
+    out, err = capfd.readouterr()
+    assert not err
+    assert out == "foo: prologue\nbar: prologue\nfoo Step.PULL\n"
+
+
+def test_callback_epilogue(tmpdir, capfd):
+    callbacks.register_epilogue(_my_exec_callback)
+
+    parts = yaml.safe_load(_exec_yaml)
+    lf = craft_parts.LifecycleManager(
+        parts, application_name="test_callback", work_dir=tmpdir, message="epilogue"
+    )
+
+    with lf.execution_context() as ctx:
+        ctx.execute(Action("foo", Step.PULL))
+
+    out, err = capfd.readouterr()
+    assert not err
+    assert out == "foo Step.PULL\nfoo: epilogue\nbar: epilogue\n"
