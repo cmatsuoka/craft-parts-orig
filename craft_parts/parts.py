bc864c6edadb4a39fc30288998723bb2395205b1
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2021-04-01 17:03:52 -0300

    Resolve import cycles
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- craft_parts/parts.py
+++ craft_parts/parts.py
@@ -25,7 +25,7 @@ from craft_parts.dirs import ProjectDirs
 from craft_parts.steps import Step
 
 if TYPE_CHECKING:
-    from craft_parts.plugins import PluginProperties
+    from craft_parts.plugins.options import PluginProperties
 
 # pylint: disable=too-many-public-methods
 # We use many property getters to prevent unintentional value overwrites
